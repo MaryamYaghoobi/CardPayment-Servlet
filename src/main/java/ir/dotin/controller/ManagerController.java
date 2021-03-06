5cb760250c0101d599485a9a87209027cdf0e190
MaryamYaghoobi / <maryam.yaghoobi@rocketmail.com>
2021-03-06 08:14:04 +0330

    Commit

--- src/main/java/ir/dotin/controller/ManagerController.java
+++ src/main/java/ir/dotin/controller/ManagerController.java
@@ -1,6 +1,7 @@
 package ir.dotin.controller;
 
 import ir.dotin.entity.CategoryElement;
+import ir.dotin.entity.Email;
 import ir.dotin.entity.Employee;
 import ir.dotin.service.*;
 import ir.dotin.share.Validation;
@@ -12,12 +13,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @WebServlet("/ManagerController")
 public class ManagerController extends HttpServlet {
+    public static final String FILE_PATH_PREFIX = "B:";
+    public static final String TOMCAT_FILE_PATH = FILE_PATH_PREFIX + "\\apache8\\apache-tomcat-8.0.0\\";
     ManagerService managerService = new ManagerService();
     LeavesService leavesService = new LeavesService();
     EmailService emailService = new EmailService();
@@ -65,6 +74,18 @@ public class ManagerController extends HttpServlet {
                 break;
             case "logout":
                 logout(request, response);
+                break;
+            case "sendMessages":
+                sendMessages(request, response);
+                break;
+            case "forwardingMessage":
+                forwardingMessage(request, response);
+                break;
+            case "ReceiveMessages":
+                ReceiveMessages(request, response);
+                break;
+            case "downloadAttachment":
+                downloadAttachment(request, response);
                 break;
         }
     }
@@ -266,6 +287,75 @@ public class ManagerController extends HttpServlet {
         request.getSession().removeAttribute("username");
         request.getSession().invalidate();
         response.sendRedirect("login.jsp");
+    }
+//---------------------------------------------------------------
+
+    public void downloadAttachment(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String fileName = request.getParameter("fileName");
+        String filePath = TOMCAT_FILE_PATH;
+        response.setContentType("APPLICATION/OCTET-STREAM");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(filePath + fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int i;
+        while ((i = fileInputStream.read()) != -1) {
+            out.write(i);
+        }
+        fileInputStream.close();
+        out.close();
+    }
+
+    public void ReceiveMessages(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Employee employee = employeeService.
+                searchUsername((String) request.getSession().getAttribute("username"));
+
+        List<Object[]> ReceiveMessages = emailService.detailsMessagesReceived(employee);
+        List<Object[]> sentMessages = emailService.detailsMessagesSent(employee);
+        request.setAttribute("ReceiveMessages", ReceiveMessages);
+        request.setAttribute("sentMessages", sentMessages);
+        RequestDispatcher rs = request.getRequestDispatcher("ReceiveMessages.jsp");
+        rs.forward(request, response);
+
+    }
+    public void sendMessages(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Employee> employeeList = employeeService.allEmployee();
+        request.setAttribute("employeeList", employeeList);
+        RequestDispatcher rs = request.getRequestDispatcher("forwardingMessage.jsp");
+        rs.forward(request, response);
+    }
+
+    public void forwardingMessage(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        Part filePart = request.getPart("file");
+        String fileName = filePart.getSubmittedFileName();
+        for (Part part : request.getParts()) {
+            part.write(TOMCAT_FILE_PATH + fileName); }
+        response.getWriter().print("The file uploaded Successfully.");
+        Employee senderEmail = employeeService.
+                searchUsername((String) request.getSession().getAttribute("username"));
+        Email email = new Email();
+        String subject = request.getParameter("subject");
+        email.setSubject(subject);
+        String message = request.getParameter("message");
+        email.setContext(message);
+        String[] employeeIds = request.getParameterValues("select");
+        List<Integer> Ids = Arrays.stream(employeeIds)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        List<Employee> receivedEmailEmployees = employeeService.receivedEmailEmployees(Ids);
+        email.getReceiverEmployees().addAll(receivedEmailEmployees);
+        emailService.addMessages(email);
+        employeeService.updateSentEmailEmployee(senderEmail, email);
+        String messages="forwardingMessage";
+        request.setAttribute(messages, true);
+        RequestDispatcher rs = request.getRequestDispatcher("employeeDashboard.jsp");
+        rs.forward(request, response);
     }
 
 }
