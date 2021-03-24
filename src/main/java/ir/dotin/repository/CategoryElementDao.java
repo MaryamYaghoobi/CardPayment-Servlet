8a2c33fd9f8e214323e8173721bf03e6f509d55c
MaryamYaghoobi / <maryam.yaghoobi@rocketmail.com>
2021-03-24 21:06:10 +0430

    CommitRefactor

--- src/main/java/ir/dotin/repository/CategoryElementDao.java
+++ src/main/java/ir/dotin/repository/CategoryElementDao.java
@@ -7,12 +7,10 @@ import org.hibernate.Session;
 import javax.persistence.Query;
 
 public class CategoryElementDao {
-
     public CategoryElement searchCategoryElement(String code) {
         CategoryElement categoryElementList = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             session.beginTransaction();
-
             Query query = session.createQuery
                     ("select c from CategoryElement c where c.code=:category");
             query.setParameter("category", code);
